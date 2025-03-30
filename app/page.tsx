d02e26a21ee75d97a4abe133343f936537906f97
Tyler Chin <twhc99@gmail.com>
2025-03-30 18:27:25 -0400

    Initial changes

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="bg-gray-600 min-h-screen flex items-center justify-center">
+      <h1 className="text-3xl font-bold underline">Hello world, my name is Tyler Chin!</h1>
+    </div>
+  );
+}
